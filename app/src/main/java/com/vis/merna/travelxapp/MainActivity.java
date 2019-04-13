055e54f4a040d995cf2707f3548653a5f89218e2
Merna <merna.hamada@vodafone.com>
2019-04-13 04:35:19 +0200

    remove unused menu

--- app/src/main/java/com/vis/merna/travelxapp/MainActivity.java
+++ app/src/main/java/com/vis/merna/travelxapp/MainActivity.java
@@ -55,26 +55,4 @@ public class MainActivity extends AppCompatActivity {
         travelViewPagerAdapter.addFragment(new HistoryFragment(), getString(R.string.history_page_title));
         viewPager.setAdapter(travelViewPagerAdapter);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
